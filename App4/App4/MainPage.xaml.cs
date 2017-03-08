3a64ea5248579d722b0a8e28caed8347595aad01
Kent Huang <kenttttt0@gmail.com>
2017-03-07 16:25:26 -0800

    heloo

---
+++ App4/App4/MainPage.xaml.cs
@@ -0,0 +1,32 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using Xamarin.Forms;
+
+namespace App4
+{
+    public partial class MainPage : ContentPage
+    {
+        public MainPage()
+        {
+            InitializeComponent();
+
+            DevicesList.ItemsSource = new List<BleDevice>
+            {
+                new BleDevice
+                {
+                    deviceName = "device1"
+                },
+
+                new  BleDevice
+                {
+                    deviceName = "device2"
+                },
+
+                new BleDevice {deviceName="device3" },
+            };
+        }
+    }
+}
